05ae4de496b331eb3b73b056fd1104bd9229b0a6
ahohjo <ilya.bylyav@yandex.ru>
2020-12-26 12:26:30 +0300

    z2

--- matrix88.py
+++
@@ -1,6 +0,0 @@
-import random
-M = int(input('M ='))
-matr = [[random.randrange(0,10) for y in range(M)] for x in range(M)]
-
-for i in range(len(matr)):
-    print([0 for j in range(i)]+ matr[i][i:])
---
+++ series29.py
@@ -0,0 +1,7 @@
+K = int(input("K ="))
+N = int(input("N ="))
+summ = 0
+for i in range(1,N+1):
+    a = int(input("a ="))
+    summ += a * K
+print(summ)
